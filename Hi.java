01757ff3e8de62fc68782dd0f244afb8ccdf34d6
muhmmddziyann-mp <muhmmdd.ziyann@gmail.com>
2026-09-25 21:18:55 +0530

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,27 @@
+//write a java program to define a car class withh properties and behaviour create objects for diffrent car models and display the details?
+
+class Car
+{
+ String model;
+ String color;
+ int year;
+ int price;
+
+void car(String m, String c, int y, int p)
+{
+    this.model=m;
+    this.color=c;
+    this.year=y;
+    this.price=p;
+}
+void displayDetails()
+{
+    System.out.println("Car Model: " + model);
+    System.out.println("Car Color: " + color);
+    System.out.println("Car Year: " + year);
+    System.out.println("Car Price:" + price);
+}
+}
+public class Main{
+    public st
+}
